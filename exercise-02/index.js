27a5332ffe5052bfa789dbe5c15a0d398525e01b
musyaffaas <syauqialharaki@gmail.com>
2025-12-30 09:52:14 +0700

    fix: correct add implementation to handle failing test cases

--- exercise-02/index.js
+++ exercise-02/index.js
@@ -1,3 +1,8 @@
-module.exports = function add(a, b) {
-  return a + b;
+module.exports = function add(... args) {
+  for (let arg of args) {
+    if (typeof arg === "string") {
+      throw new Error("Cannot call add with string");
+    }
+  }
+  return args.reduce((sum, num) => sum + num, 0);
 };
